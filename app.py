21b4d5314b1efe2921d6fb638d0895b0f0b24262
Rajeev PR <rajeevpeeaar@gmail.com>
2025-06-18 02:33:03 +0530

    new

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+import os
 
 app = Flask(__name__)
-DB = 'data.db'
+DB = os.path.join('/tmp', 'data.db')
 
 def init_db():
     with sqlite3.connect(DB) as conn:
@@ -32,4 +33,5 @@ def delete(id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
